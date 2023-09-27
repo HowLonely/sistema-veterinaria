44712413be7e96920151feec58b9c30190dba2a0
HowLonely <ellian.andre1203@gmail.com>
2023-09-26 22:00:27 -0300

    Actualización mascotas

--- veterinaria/models.py
+++ veterinaria/models.py
@@ -6,20 +6,18 @@ clientes = [
     {'rut': '17389832-2', 'nombre': 'juan', 'apellidos': 'toledo gonzalez', 'fono_1': '912893829', 'fono_2': '912832928', 'email': 'juan.tg@correo.cl'},
     {'rut': '19239843-1', 'nombre': 'camila', 'apellidos': 'vargas salazar', 'fono_1': '932387645', 'fono_2': '966476663', 'email': 'camila.vs@correo.cl'},
 ]
-
 # Foreign key son las posiciones en los arreglos. ej: raza:0 -> razas[0]
-mascotas = [
-    {'sexo': 0, 'edad_meses': 54, 'nombre': 'blanca', 'num_chip': 120030, 'raza': 0, 'dueno': 0},
-    {'sexo': 1, 'edad_meses': 24, 'nombre': 'negro', 'num_chip': 100293, 'raza': 1, 'dueno': 2},
-    {'sexo': 0, 'edad_meses': 75, 'nombre': 'homero', 'num_chip': None, 'raza': 2, 'dueno': 1},
-]
-
 razas = [
     {'nombre_raza': 'pastor aleman', 'especie': 'perro'},
     {'nombre_raza': 'angora', 'especie' : 'gato'},
     {'nombre_raza': 'loro gris africano', 'especie': 'loro'},
 ]
 
+mascotas = [
+    {'sexo': 0, 'edad_meses': 54, 'nombre': 'blanca', 'num_chip': 120030, 'raza': razas[0], 'dueno': clientes[0]},
+    {'sexo': 1, 'edad_meses': 24, 'nombre': 'negro', 'num_chip': 100293, 'raza': razas[1], 'dueno': clientes[2]},
+    {'sexo': 0, 'edad_meses': 75, 'nombre': 'homero', 'num_chip': None, 'raza': razas[2], 'dueno': clientes[1]},
+]
 
 veterinarios = [
     # rut, credencial, nombre, apellidos, fono, email
