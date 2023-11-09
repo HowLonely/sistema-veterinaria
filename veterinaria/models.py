c605b2b76fb443add313c019201ca5eb46a527cc
HowLonely <ellian.andre1203@gmail.com>
2023-11-08 21:37:39 -0300

    models listos y .gitignore

--- veterinaria/models.py
+++ veterinaria/models.py
@@ -1,34 +1,101 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
-clientes = [
-    {'rut': '18789374-1', 'nombre': 'pedro', 'apellidos': 'diaz rojas', 'fono_1': '932384983', 'fono_2': '923784732', 'email': 'pedro.dr@correo.cl'},
-    {'rut': '17389832-2', 'nombre': 'juan', 'apellidos': 'toledo gonzalez', 'fono_1': '912893829', 'fono_2': '912832928', 'email': 'juan.tg@correo.cl'},
-    {'rut': '19239843-1', 'nombre': 'camila', 'apellidos': 'vargas salazar', 'fono_1': '932387645', 'fono_2': '966476663', 'email': 'camila.vs@correo.cl'},
-]
-# Foreign key son las posiciones en los arreglos. ej: raza:0 -> razas[0]
-razas = [
-    {'nombre_raza': 'pastor aleman', 'especie': 'perro'},
-    {'nombre_raza': 'angora', 'especie' : 'gato'},
-    {'nombre_raza': 'loro gris africano', 'especie': 'loro'},
-]
 
-mascotas = [
-    {'sexo': 0, 'edad_meses': 54, 'nombre': 'blanca', 'num_chip': 120030, 'raza': razas[0], 'dueno': clientes[0]},
-    {'sexo': 1, 'edad_meses': 24, 'nombre': 'negro', 'num_chip': 100293, 'raza': razas[1], 'dueno': clientes[2]},
-    {'sexo': 1, 'edad_meses': 75, 'nombre': 'homero', 'num_chip': None, 'raza': razas[2], 'dueno': clientes[1]},
-]
+class Asistentes(models.Model):
+    id_asistente = models.AutoField(primary_key=True)
+    nombres = models.CharField(max_length=145, blank=True, null=True)
+    apellidos = models.CharField(max_length=145, blank=True, null=True)
+    fono = models.CharField(max_length=14, blank=True, null=True)
+    email = models.CharField(max_length=245, blank=True, null=True)
+    credenciales = models.CharField(max_length=145, blank=True, null=True)
 
-veterinarios = [
-    # rut, credencial, nombre, apellidos, fono, email
-]
+    class Meta:
+        managed = False
+        db_table = 'asistentes'
 
-fichas_clinicas = [
-    {'mascota': mascotas[0], 'veterinario': veterinarios[0]}
-]
 
-atenciones = [
-    {'cod_atencion': 123, 'hora_inicio': '12:15', 'hora_termino': '13:00', 'veterinario': 'Snoop Dogg', 'fecha_atencion': '15/09/2023','mascota_atendida': mascotas[0] , 'diagnostico': 'Enfermedad por estrés', 'tratamiento': 'Acostarse mas temprano','observaciones': None, 'fechas_control': '02/10/2023'},
-    {'cod_atencion': 456, 'hora_inicio': '10:30', 'hora_termino': '11:00', 'veterinario': 'Bad Bunny', 'fecha_atencion': '10/08/2023','mascota_atendida': mascotas[1],'diagnostico': 'Pie plano', 'tratamiento': 'Reposo','observaciones': 'Evitar esfuerzo fisico', 'fechas_control': '06/09/2023'},
-    {'cod_atencion': 789, 'hora_inicio': '15:00', 'hora_termino': '15:15', 'veterinario': 'Pablo Chill-E', 'fecha_atencion': '15/09/2023','mascota_atendida': mascotas[2], 'diagnostico': 'Irritación en el ojo', 'tratamiento': 'Gotas para los ojos a diario','observaciones': 'Evitar estar en ambientes áridos', 'fechas_control': '30/09/2023'},
-]
+class Atenciones(models.Model):
+    id_atencion = models.AutoField(primary_key=True)
+    hora_ingreso = models.TimeField(blank=True, null=True)
+    hora_termino = models.TimeField(blank=True, null=True)
+    id_ficha_clinica = models.ForeignKey('FichasClinicas', models.DO_NOTHING, db_column='id_ficha_clinica', blank=True, null=True)
+    id_veterinario = models.ForeignKey('Veterinarios', models.DO_NOTHING, db_column='id_veterinario', blank=True, null=True)
+    diagnostico = models.CharField(max_length=280, blank=True, null=True)
+    tratamiento = models.CharField(max_length=280, blank=True, null=True)
+    observaciones = models.CharField(max_length=245, blank=True, null=True)
+    fecha_atencion = models.DateField(blank=True, null=True)
+    agendada_por_asistente = models.ForeignKey(Asistentes, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'atenciones'
+
+
+class Clientes(models.Model):
+    id_cliente = models.AutoField(primary_key=True)
+    rut = models.CharField(max_length=10, blank=True, null=True)
+    nombres = models.CharField(max_length=145, blank=True, null=True)
+    apellidos = models.CharField(max_length=145, blank=True, null=True)
+    fono_1 = models.CharField(max_length=14, blank=True, null=True)
+    fono_2 = models.CharField(max_length=14, blank=True, null=True)
+    email = models.CharField(max_length=245, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'clientes'
+
+
+class FichasClinicas(models.Model):
+    id_ficha_clinica = models.AutoField(primary_key=True)
+    id_mascota = models.ForeignKey('Mascotas', models.DO_NOTHING, db_column='id_mascota', blank=True, null=True)
+    id_veterinario = models.ForeignKey('Veterinarios', models.DO_NOTHING, db_column='id_veterinario', blank=True, null=True)
+    fecha_ingreso = models.DateField(blank=True, null=True)
+    creada_por_asistente = models.ForeignKey(Asistentes, models.DO_NOTHING, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'fichas_clinicas'
+
+
+class Mascotas(models.Model):
+    id_mascota = models.AutoField(primary_key=True)
+    id_dueno = models.ForeignKey(Clientes, models.DO_NOTHING, db_column='id_dueno', blank=True, null=True)
+    nombres = models.CharField(max_length=145, blank=True, null=True)
+    num_chip = models.CharField(max_length=145, blank=True, null=True)
+    id_raza = models.ForeignKey('Razas', models.DO_NOTHING, db_column='id_raza', blank=True, null=True)
+    edad_meses = models.IntegerField(blank=True, null=True)
+    sexo = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'mascotas'
+
+
+class Razas(models.Model):
+    id_raza = models.AutoField(primary_key=True)
+    nombre_raza = models.CharField(max_length=85, blank=True, null=True)
+    nombre_especie = models.CharField(max_length=85, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'razas'
+
+
+class Veterinarios(models.Model):
+    id_veterinario = models.AutoField(primary_key=True)
+    nombres = models.CharField(max_length=145, blank=True, null=True)
+    apellidos = models.CharField(max_length=145, blank=True, null=True)
+    credenciales = models.CharField(max_length=145, blank=True, null=True)
+    fono = models.CharField(max_length=14, blank=True, null=True)
+    email = models.CharField(max_length=245, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'veterinarios'
